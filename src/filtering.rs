016de008ad8369689d4cd9176582bf6fb2e6303b
Takayuki Maeda <takoyaki0316@gmail.com>
2020-12-30 03:48:23 +0900

    fetch different events in different functions

--- src/filtering.rs
+++ src/filtering.rs
@@ -1,13 +1,13 @@
-use crate::event::{Commit, Event, PullRequest, PullRequestStatus, Repository};
+use crate::event::{self, Event, PullRequest, PullRequestStatus, Repository};
 use regex::Regex;
 use scraper::{Html, Selector};
 use std::error::Error;
 
-const TIMELINE_BODY: &str = "div.TimelineItem-body";
-const ACTIVITY_NAME: &str = "summary span.color-text-primary.ws-normal.text-left";
-const OPENED_PULL_REQUEST_REPOSITORY: &str = "summary div span";
-const REVIEWED_PULL_REQUEST_REPOSITORY: &str = "summary span span";
-const STATUS: &str = "ul li svg";
+pub(crate) const TIMELINE_BODY: &str = "div.TimelineItem-body";
+pub(crate) const EVENT_NAME: &str = "summary span.color-text-primary.ws-normal.text-left";
+pub(crate) const OPENED_PULL_REQUEST_REPOSITORY: &str = "summary div span";
+pub(crate) const REVIEWED_PULL_REQUEST_REPOSITORY: &str = "summary span span";
+pub(crate) const STATUS: &str = "ul li svg";
 
 #[derive(Debug, PartialEq)]
 enum WadachiError {
@@ -67,24 +67,15 @@ impl Filtering {
         }
 
         let timeline_body = Selector::parse(TIMELINE_BODY).unwrap();
-        let activity = Selector::parse(ACTIVITY_NAME).unwrap();
+        let activity = Selector::parse(EVENT_NAME).unwrap();
         let pull_requests = Selector::parse("details details").unwrap();
         let a = Selector::parse("ul li a").unwrap();
         let status = Selector::parse(STATUS).unwrap();
         let date = Selector::parse("time").unwrap();
 
-        let mut activities = vec![];
-        let mut res = surf::get(
-            format!(
-                "https://github.com/{}?tab=overview&from={}-{}-01&to={}-{}-30",
-                self.user, self.from.year, self.from.month, self.to.year, self.from.month
-            )
-            .as_str(),
-        )
-        .await?;
-        let document = Html::parse_document(res.body_string().await?.as_str());
-        for element in document.select(&timeline_body) {
-            let activity_name = if let Some(activity) = element.select(&activity).next() {
+        let mut events = vec![];
+        for element in self.document().await?.select(&timeline_body) {
+            let event_name = if let Some(activity) = element.select(&activity).next() {
                 activity
                     .text()
                     .collect::<String>()
@@ -104,32 +95,18 @@ impl Filtering {
 
             if Regex::new(r"^Created \d{1,} commits in \d{1,} repositor(y|ies)$")
                 .unwrap()
-                .is_match(activity_name.as_str())
+                .is_match(event_name.as_str())
             {
-                let repository_iter = element.select(&a).step_by(2);
-                let commit_iter = element.select(&a).skip(1).step_by(2);
-                for (repository, commit) in repository_iter.zip(commit_iter) {
-                    activities.push(Event::CreateCommit {
-                        name: activity_name.clone(),
-                        commit: Commit {
-                            repository: Repository {
-                                name: repository.text().collect::<String>().trim().to_string(),
-                                url: repository.value().attr("href").unwrap().trim().to_string(),
-                            },
-                            number: 3,
-                            url: commit.value().attr("href").unwrap().trim().to_string(),
-                        },
-                    })
-                }
+                event::fetch_push_event(element, &mut events, event_name);
             } else if Regex::new(r"^Created \d{1,} repositor(y|ies)$")
                 .unwrap()
-                .is_match(activity_name.as_str())
+                .is_match(event_name.as_str())
             {
                 let repository_iter = element.select(&a);
                 let date_iter = element.select(&date);
                 for (repository, date) in repository_iter.zip(date_iter) {
-                    activities.push(Event::CreateRepository {
-                        name: activity_name.clone(),
+                    events.push(Event::CreateRepository {
+                        name: event_name.clone(),
                         repository: Repository {
                             name: repository.text().collect::<String>().trim().to_string(),
                             url: repository.value().attr("href").unwrap().trim().to_string(),
@@ -141,96 +118,30 @@ impl Filtering {
                 r"^Opened \d{1,} (other )?pull request(s)? in \d{1,} repositor(y|ies)$",
             )
             .unwrap()
-            .is_match(activity_name.as_str())
+            .is_match(event_name.as_str())
             {
-                for repository in element.select(&pull_requests) {
-                    let repository_name = repository
-                        .select(&Selector::parse(OPENED_PULL_REQUEST_REPOSITORY).unwrap())
-                        .next()
-                        .unwrap()
-                        .text()
-                        .collect::<String>()
-                        .trim()
-                        .to_string();
-                    let pull_request_iter = repository.select(&a);
-                    let status_iter = repository.select(&status);
-                    let date_iter = repository.select(&date);
-                    for ((pull_request, date), status) in
-                        pull_request_iter.zip(date_iter).zip(status_iter)
-                    {
-                        activities.push(Event::CreatePullRequest {
-                            name: activity_name.clone(),
-                            repository: Repository {
-                                name: repository_name.clone(),
-                                url: format!("/{}", repository_name.clone()),
-                            },
-                            pull_request: PullRequest {
-                                name: pull_request.text().collect::<String>().trim().to_string(),
-                                url: pull_request
-                                    .value()
-                                    .attr("href")
-                                    .unwrap()
-                                    .trim()
-                                    .to_string(),
-                            },
-                            status: if status.value().attr("class").unwrap().contains("text-green")
-                            {
-                                PullRequestStatus::Opened
-                            } else {
-                                PullRequestStatus::Closed
-                            },
-                            created_at: date.text().collect::<String>().trim().to_string(),
-                        })
-                    }
-                }
+                event::fetch_pull_request_event(element, &mut events, event_name)
             } else if Regex::new(r"^Reviewed \d{1,} pull request(s)? in \d{1,} repositor(y|ies)$")
                 .unwrap()
-                .is_match(activity_name.as_str())
+                .is_match(event_name.as_str())
             {
-                for repository in element.select(&pull_requests) {
-                    let repository_name = repository
-                        .select(&Selector::parse(REVIEWED_PULL_REQUEST_REPOSITORY).unwrap())
-                        .next()
-                        .unwrap()
-                        .text()
-                        .collect::<String>()
-                        .trim()
-                        .to_string();
-                    let pull_request_iter = repository.select(&a);
-                    let status_iter = repository.select(&status);
-                    let date_iter = repository.select(&date);
-                    for ((pull_request, date), status) in
-                        pull_request_iter.zip(date_iter).zip(status_iter)
-                    {
-                        activities.push(Event::ReviewPullRequest {
-                            name: activity_name.clone(),
-                            repository: Repository {
-                                name: repository_name.clone(),
-                                url: format!("/{}", repository_name.clone()),
-                            },
-                            pull_request: PullRequest {
-                                name: pull_request.text().collect::<String>().trim().to_string(),
-                                url: pull_request
-                                    .value()
-                                    .attr("href")
-                                    .unwrap()
-                                    .trim()
-                                    .to_string(),
-                            },
-                            status: if status.value().attr("class").unwrap().contains("text-green")
-                            {
-                                PullRequestStatus::Opened
-                            } else {
-                                PullRequestStatus::Closed
-                            },
-                            created_at: date.text().collect::<String>().trim().to_string(),
-                        })
-                    }
-                }
+                event::fetch_pull_request_review_event(element, &mut events, event_name)
             }
         }
 
-        Ok(activities)
+        Ok(events)
+    }
+
+    async fn document(&self) -> Result<Html, Box<dyn Error>> {
+        let mut res = surf::get(
+            format!(
+                "https://github.com/{}?tab=overview&from={}-{}-01&to={}-{}-30",
+                self.user, self.from.year, self.from.month, self.to.year, self.from.month
+            )
+            .as_str(),
+        )
+        .await?;
+        Ok(Html::parse_document(res.body_string().await?.as_str()))
     }
 }
 
