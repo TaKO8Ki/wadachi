f705d8ff39e8bf5c364532e5aeddc732f57a7c1d
Takayuki Maeda <takoyaki0316@gmail.com>
2021-01-12 22:15:11 +0900

    cli config

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{App, Arg};
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = App::new("wadachi")
+        .version("1.0")
+        .author("Takayuki Maeda <takoyaki0316@gmail.com>")
+        .about("wadachi scrapes your GitHub Activities")
+        .arg(
+            Arg::with_name("NAME")
+                .help("Sets the GitHub user name")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+    let activities = wadachi::new(matches.value_of("NAME").unwrap())
+        .from(2020, 12, 1)
+        .execute()
+        .await?;
+    for activity in activities {
+        match activity {
+            wadachi::event::Event::PullRequest { repository, .. } => {
+                println!("repository: {:?}", repository)
+            }
+            _ => (),
+        }
+    }
+    Ok(())
 }
